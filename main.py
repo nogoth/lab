5f6d62225a0c658d88eb676746c88c00e4dffe9b
Benjamin Livingood <livingood@gmail.com>
2025-09-16 14:35:09 -0600

    voice search

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pyttsx3
+
 def main():
-    print("Hello from github-lab!")
+
     engine = pyttsx3.init()
-    engine.setProperty('rate', 125)
+    engine.setProperty('rate', 70)
     engine.setProperty('volume', 1.0)
-    engine.say("Testing")
+    engine.setProperty('voice', 'mb-en1')
+    engine.setProperty('voice', 'en')
+
+    voices = engine.getProperty('voices')
+    print(voices[0])
+    printVoices(voices)
+
+#    engine.setProperty('voice', 'us-mbrola-1')
+    engine.say("this a test of the engine and it can say things")
     engine.runAndWait()
-    pyttsx3.speak("another brilliant try")
-    print("should have said something")
+    # example of just running with a speak, engine is a global so it'll remember what was set up
+    pyttsx3.speak("what is that you need grace in")
+    engine.stop()
+
+def printVoices(voicesArray):
+    map(lambda x: print(x), voicesArray)
+    for x in voicesArray:
+        print(x)
 
 
 if __name__ == "__main__":
