2db05e47909e097ba047e070ec773b6b21d5611b
Benjamin Livingood <livingood@gmail.com>
2025-09-18 15:07:27 -0600

    tts try again

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage
 import re
 
+import torch
+from TTS.api import TTS
+import sounddevice as sd
+
+
+def speak_it(content: str):
+  # TODO: abstract this tts set up out of a single method...
+  # cargo cult, might not really care to try and cuda since laptop is intel gpu
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  tts = TTS("tts_models/en/ljspeech/glow-tts")#.to(device)
+  wav = tts.tts(text=content)
+  sd.play(wav, samplerate=22050)
+  sd.wait() # Wait until file is done playing
+
 def removeThinks(message: AIMessage):
     regex_pattern = r'<think>[\s\S]*?</think>'
     return  re.sub(regex_pattern, '', message)
@@ -13,10 +27,14 @@ def talkToServer():
   # note: chatollama works with servers that are ollama starters not mozilla-ocho/llama.cpp servers
   model = "llava-v1.5-7b"
   model = "qwen3:1.7b"
-  llm = ChatOllama(model=model, temperature=0.7) # reasoning=False) # for not think
-  response = llm.invoke([HumanMessage(content="Hello, how are you?")])
+  temperature = 2.2
+  llm = ChatOllama(model=model, temperature=temperature) # reasoning=False) # for not think
+  response = llm.invoke([HumanMessage(content="Tell me a story, in the style of Hansel and Gretel, that is at least 30 sentences long")])
   print(removeThinks(response.content))
   print(response.content)
+  #now say it... i hope
+  speak_it(removeThinks(response.content))
+
 
 talkToServer()
 
---
+++ test.py
@@ -0,0 +1,19 @@
+import torch
+import sounddevice as sd
+from TTS.api import TTS
+
+# Get device. You can specify a different device if needed.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Init TTS
+tts = TTS("tts_models/en/ljspeech/glow-tts").to(device)
+
+# The text you want to synthesize
+text_to_speak = "This is a test of the Coqui TTS library."
+
+# Generate audio data
+wav = tts.tts(text=text_to_speak)
+
+# Play the audio
+sd.play(wav, samplerate=22050)
+sd.wait() # Wait until file is done playing
