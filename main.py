460c9b7a7fefe0759cd2fb2533d0ea627f67f541
Benjamin Livingood <livingood@gmail.com>
2025-09-18 15:07:27 -0600

    tts try again

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage
 import re
 
+import torch
+from TTS.api import TTS
+import sounddevice as sd
+
+
+def speak_it(content: str):
+  model = "tts_models/en/ljspeech/glow-tts"
+  model = "tts_models/en/vctk/vits"
+  # TODO: abstract this tts set up out of a single method...
+  # cargo cult, might not really care to try and cuda since laptop is intel gpu
+  device = "cuda" if torch.cuda.is_available() else "cpu"
+  tts = TTS(model)#.to(device)
+  wav = tts.tts(text=content, speaker="p330")
+  sd.play(wav, samplerate=22050)
+  sd.wait() # Wait until file is done playing
+
 def removeThinks(message: AIMessage):
     regex_pattern = r'<think>[\s\S]*?</think>'
     return  re.sub(regex_pattern, '', message)
@@ -13,10 +29,16 @@ def talkToServer():
   # note: chatollama works with servers that are ollama starters not mozilla-ocho/llama.cpp servers
   model = "llava-v1.5-7b"
   model = "qwen3:1.7b"
-  llm = ChatOllama(model=model, temperature=0.7) # reasoning=False) # for not think
-  response = llm.invoke([HumanMessage(content="Hello, how are you?")])
+  temperature = 9.2
+  llm = ChatOllama(model=model, temperature=temperature) # reasoning=False) # for not think
+  response = llm.invoke([HumanMessage(content="Tell me a story, in the style of Hansel and Gretel, that is at least 30 sentences long")])
+  #response = llm.invoke([HumanMessage(content="hi")])
   print(removeThinks(response.content))
   print(response.content)
+  input("Ok, ready to tell you a story")
+  #now say it... i hope
+  speak_it(removeThinks(response.content))
+
 
 talkToServer()
 
