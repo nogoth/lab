97632618e892f1e09cbe180e27b65e13706b63c0
Benjamin Livingood <livingood@gmail.com>
2025-10-10 13:50:19 -0600

    um

---
+++ one-offs/musings.py
@@ -0,0 +1,19 @@
+from typing import Self # 3.11  or higher
+
+class LinkNode():
+    def __init__(self,input=None,ptr=None):
+        self.value=input
+        self.next=ptr
+    def add(self, ptr=None) -> Self:
+        if not ptr is None:
+            self.next = ptr
+        return self
+
+h = LinkNode(1)
+n1 = LinkNode(2,h)
+n2 = LinkNode(3,n1).add(LinkNode(14))
+
+
+print(h)
+print(n1.next)
+print(n2.next)
