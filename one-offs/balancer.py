9feaa8d16fe252a2a7ef182ce03c6432656e37ec
Benjamin Livingood <livingood@gmail.com>
2025-11-14 10:28:07 -0700

    balance

--- one-offs/balancer.py
+++ one-offs/balancer.py
@@ -1,9 +1,10 @@
 import random
 
 arr = [ random.randint(-100,120) for x in range(1000) ]
-
+# example given says take a file, read each line, it will be of a kind of each line = 1,2,3,4\n1,4,5\n
 def balancer(nums: list[int]) -> list(list[int]):
     # given an array partition it into two balanced sides, balanced defined as the sum of the elements
+    # preserve order however, numbers can be negative, it will fit all in memory, partitions will likely be differing lengths
     # it's a two tail
     i = 0
     j = len(nums)-1
@@ -25,6 +26,35 @@ def balancer(nums: list[int]) -> list(list[int]):
     else:
         return []
 
-print(f"{balancer([1,2,3,4])}")
-print(f"{balancer([1,4,5])}")
-print(f"{balancer(arr)}")
+def validate_string(raw_string: str) -> bool:
+    #define a class of items that are allowed
+    #1-9,-,' '
+    # could take the string, split it into each char and then uniq the compare, heavy lifting though
+    # guess we can just iterate over the split and if it throws an error popout
+    for val in raw_string.split(","): #granted we now have two splits... fix later
+        try:
+            v = int(val)
+        except ValueError:
+            return False
+    return True
+
+def turn_array(raw_string: str) -> list:
+    if validate_string(raw_string):
+        return [ int(x) for x in raw_string.split(",") ] 
+    return []
+
+#print(f"{balancer([1,2,3,4])}")
+#print(f"{balancer([1,4,5])}")
+#print(f"{balancer(arr)}")
+
+#- print(f"{turn_array('1,2,3,4,10,01,-1')}")
+#- print(f"{turn_array('1')}")
+#- print(f"{turn_array('1,a')}")
+#- print(f"{turn_array('\n')}")
+
+
+def dontdothis():
+    import sys
+
+    for x in sys.stdin:
+        print(f"{x.strip()}")
