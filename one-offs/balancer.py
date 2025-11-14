4a39e2f1ca812d7bf764ce8b17db994f8ad27d5d
Benjamin Livingood <livingood@gmail.com>
2025-11-14 10:28:07 -0700

    balance

--- one-offs/balancer.py
+++ one-offs/balancer.py
@@ -4,6 +4,7 @@ arr = [ random.randint(-100,120) for x in range(1000) ]
 
 def balancer(nums: list[int]) -> list(list[int]):
     # given an array partition it into two balanced sides, balanced defined as the sum of the elements
+    # preserve order however, numbers can be negative, it will fit all in memory, partitions will likely be differing lengths
     # it's a two tail
     i = 0
     j = len(nums)-1
