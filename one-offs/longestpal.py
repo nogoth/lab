9b7ca1c1bc43495d4c207838f9457eac66043d6f
Benjamin Livingood <livingood@gmail.com>
2025-11-22 13:26:20 -0700

    lltry2

--- one-offs/longestpal.py
+++ one-offs/longestpal.py
@@ -1,24 +1,35 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        fright, fleft = -1,-1
+        fleft = -1
         rightmax=len(s) #-1 #maxleft is 0
         maxlength = -1
         
-        for i,val in enumerate(s):
-            left=i
-            right=i
+        #evens case
+        for i in range(len(s)):
+            left = i
+            right = i
 
-            while left > -1 and right < rightmax:
-                if s[left] == s[right]:
-                    if right - left + 1 > maxlength:
-                        maxlength = right - left + 1
-                        fleft = left
-                    right += 1
-                else: #left char != right char, reduce left
-                    left -= 1
-#            print(f"maxlegnth {maxlength} fleft {fleft}")
-            
+            while left >= 0 and right < rightmax and s[left] == s[right]:
+                if right - left + 1 > maxlength:
+                    maxlength = right - left + 1
+                    fleft = left
+                right += 1
+                left -= 1
+
+        #odds case
+        for i in range(len(s)):
+            left = i
+            right = i+1
+
+            while left >= 0 and right < rightmax and s[left] == s[right]:
+                if right - left + 1 > maxlength:
+                    maxlength = right - left + 1
+                    fleft = left
+                right += 1
+                left -= 1
+
+           
         return f"{s[fleft:fleft + maxlength]}"
 
 
